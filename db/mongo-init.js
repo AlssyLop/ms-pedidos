8816b9cf7d04b80bff4a8bd55787a9cf36c5fda8
AlssyLop <alcibiades.lopezf@gmail.com>
2026-06-08 01:45:11 -0500

    fix(config): unificar URI MongoDB a base de datos plazoleta

--- db/mongo-init.js
+++ db/mongo-init.js
@@ -1,5 +1,7 @@
-db = db.getSiblingDB('plazoleta_trazabilidad');
+db = db.getSiblingDB('plazoleta');
 
 db.createCollection('trazabilidad');
 
 db.trazabilidad.createIndex({ idPedido: 1, fechaCambio: 1 });
+db.trazabilidad.createIndex({ idRestaurante: 1, fechaCambio: 1 });
+db.trazabilidad.createIndex({ idCliente: 1, idPedido: 1 });
